7b5ff640097d3c90b452612543be6912df21ae03
Vincent DELOFFRE <kratos_angel_59@hotmail.fr>
2023-04-11 17:05:13 +0200

    modification des programmess pour qu'ils fonctionnent toujours

--- exo_13.rb
+++ exo_13.rb
@@ -1,9 +1,9 @@
 mails = Array.new()
 50.times do |i|
     if i <9
-        mails[i] = "jean.dupont.0#{i+1}@email.fr\n"
+        mails[i] = "jean.dupont.0#{i+1}@email.fr"
     else
-        mails[i] = "jean.dupont.#{i+1}@email.fr\n"
+        mails[i] = "jean.dupont.#{i+1}@email.fr"
     end
 end
 puts mails
